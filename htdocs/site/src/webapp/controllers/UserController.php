c14d9e22b5ef17df1acb2e35ce42ee66bc30a0cd
emilte <emilte@stud.ntnu.no>
2019-03-19 12:02:46 +0100

    Add semicolon

--- htdocs/site/src/webapp/controllers/UserController.php
+++ htdocs/site/src/webapp/controllers/UserController.php
@@ -36,7 +36,7 @@ class UserController extends Controller
 
         if($request->post('email'))
         {
-          $email = $request->post('email')
+          $email = $request->post('email');
           $user->setEmail($email);
         }
         if($request->post('bio'))
